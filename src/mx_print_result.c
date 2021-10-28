8fe34b1e7988d2fd929eed57ae65b7158628f2f0
Andrii Yevtushenko <ayevtushen@c1r7p5.khpi.ucode-connect.study>
2021-10-28 16:16:09 +0300

    second path better

--- src/mx_print_result.c
+++ src/mx_print_result.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 void mx_print_logic(int **matrix,int size, int **result, char **islands) {
+    //create once matrix 
+    int ** once_matrix = (int**)malloc(size * sizeof(int*));
+
+    for (int i = 0; i < size; i++) {
+        once_matrix[i] = (int *)malloc(size * sizeof(int));
+    }
+    set_matrix_zero(matrix, once_matrix, size);
+
     // create copy matrix
     int **copy_matrix = (int**)malloc(size * sizeof(int*));
 
@@ -9,8 +17,17 @@ void mx_print_logic(int **matrix,int size, int **result, char **islands) {
         copy_matrix[i] = (int *)malloc(size * sizeof(int));
     }
     set_matrix_zero(matrix, copy_matrix, size);
-    // create copy result
     
+    // create copy result
+    int ** once_result = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        once_result[i] = (int *)malloc(size * sizeof(int));
+    }
+    set_result_zero(once_result, size);
+
+
+    mx_finder(once_matrix,size, once_result);
+
     int tmp = 0;
     int deep = 0;
     for (int i = 0; i < size; i++) {
@@ -19,42 +36,34 @@ void mx_print_logic(int **matrix,int size, int **result, char **islands) {
             if (i != j) {
                 set_matrix_zero(matrix, copy_matrix, size);
                 set_result_zero(result, size);
-                mx_finder(copy_matrix,size, result);
-                mx_print_result(copy_matrix, result, islands, i, j);
+                
+                mx_print_result(once_matrix, once_result, islands, i, j);
 
-                tmp = result[i][j];
+                tmp = once_result[i][j];
                 while (tmp != 0) {
-                    tmp = result[i][tmp];
+                    tmp = once_result[i][tmp];
                     deep++;
                 }
-                //printf("DEEP = %d\n", deep);
                 
                 for (int x = deep; x > 0; x--) {
                     
                     
-                    tmp = result[i][j];
+                    tmp = once_result[i][j];
                     for (int y = x; y - 1 > 0; y--) {
-                        tmp = result[i][tmp];
+                        tmp = once_result[i][tmp];
                     }
-                    // printf("TMP = %d\n", tmp);
-                    // printf("matrix = %d\n", copy_matrix[i][tmp]);
-                    // printf("matrix = %d\n", copy_matrix[tmp][i]);
                     set_result_zero(result, size);
                     set_matrix_zero(matrix, copy_matrix, size);
-                    //PRINT MATRIX
+
                     copy_matrix[i][tmp] = 999;
                     copy_matrix[tmp][i] = 999;
-                    // for (int h = 0; h < size; h++) {
-                    //     for (int l = 0; l < size; l++) {
-                    //         printf("%d\t", copy_matrix[h][l]);
-                    //     }
-                    //     printf("\n");
-                    // }
 
 
                     mx_finder(copy_matrix, size, result);
-                    if (matrix[i][j] == copy_matrix[i][j]) {
+                    if (once_matrix[i][j] == copy_matrix[i][j]) {
+                        printf("SECOND PRINT\n");
                         mx_print_result(copy_matrix, result, islands, i, j);
+                        break;
                     }
 
                 }
